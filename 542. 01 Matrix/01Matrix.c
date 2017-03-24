99ce77944cb7a3abba3778f42105a84cc507f926
strangenaiad <strange.naiad@gmail.com>
2017-03-25 01:59:17 +0800

    fix submitted version

--- 542. 01 Matrix/01Matrix.c
+++ 542. 01 Matrix/01Matrix.c
@@ -5,81 +5,70 @@
  */
 
 struct Pos {
-    int row;
-    int col;
+    int index;
     int dist;
 };
 
-void cleanScore( int rows, int cols, int**score )
-{
-    for( int row=0; row<rows; row++ ) {
-        for( int col=0; col<cols; col++ ) {
-            score[row][col] = 0x80000000;
-        }
-    }
-}
+
+struct Pos queue[900000] = {0};
+int        qi = 0;
+int        qn = 0;
 
 
-void testScore( int row, int col, int rows, int cols, int**score, int**result ) {
-    struct Pos queue[20000] = {0};
-    int        qi = 0;
-    int        qn = 0;
-    int      dist = 0;
+void testScore( int rows, int cols, int**result ) {
     
-    queue[qn].row = row;
-    queue[qn].col = col;
-    queue[qn].dist = 0;
-    qn++;
+    int      dist = 0;
+    int      index = 0;
+
+
     
     
     while( qi<qn ) {
-        row = queue[qi].row;
-        col = queue[qi].col;
+        index = queue[qi].index;
+        int row = index/cols;
+        int col = index%cols;
         dist = queue[qi].dist;
+        //printf( __FILE__ "(%d): %d/%d %d [%d][%d] %d\n", __LINE__, qi, qn, index, row, col, dist );
 
-        //printf( __FILE__ "(%d): %d/%d [%d][%d] %d\n", __LINE__, qi, qn, row, col, dist );
 
         qi++;
         if( qi==qn ) {
             qi = qn = 0;
         }
 
+        if( result[row][col]>=0 && dist>result[row][col] )
+            continue;
+            
+        //if( result[row][col]>=0 && dist-1>result[row][col] )
+        //    continue;
 
         if( result[row][col] > dist || result[row][col]<0 )
             result[row][col] = dist;
 
         // UP
-        if( row>0 && score[row-1][col]&0x80000000 ) {
-            score[row-1][col] &=~0x80000000;
-            queue[qn].row = row-1;
-            queue[qn].col = col;
+        if( row>0 ) {
+            queue[qn].index = index-cols;
             queue[qn].dist = dist+1;
             qn++;
         }
         
         // RIGHT
-        if( col+1<cols && score[row][col+1]&0x80000000 ) {
-            score[row][col+1] &=~0x80000000;
-            queue[qn].row = row;
-            queue[qn].col = col+1;
+        if( col+1<cols ) {
+            queue[qn].index = index+1;
             queue[qn].dist = dist+1;
             qn++;
         }
         
         // DOWN
-        if( row+1<rows && score[row+1][col]&0x80000000 ) {
-            score[row+1][col] &=~0x80000000;
-            queue[qn].row = row+1;
-            queue[qn].col = col;
+        if( row+1<rows ) {
+            queue[qn].index = index+cols;
             queue[qn].dist = dist+1;
             qn++;
         }
         
         // LEFT
-        if( col>0 && score[row][col-1]&0x80000000 ) {
-            score[row][col-1] &=~0x80000000;
-            queue[qn].row = row;
-            queue[qn].col = col-1;
+        if( col>0 ) {
+            queue[qn].index = index-1;
             queue[qn].dist = dist+1;
             qn++;
         }
@@ -87,43 +76,38 @@ void testScore( int row, int col, int rows, int cols, int**score, int**result ) 
 }
 
 
-
 int** updateMatrix(int** matrix, int matrixRowSize, int *matrixColSizes, int** columnSizes, int* returnSize) {
     int **result = (int**) calloc( matrixRowSize, sizeof(int*));
-    int **score  = (int**) calloc( matrixRowSize, sizeof(int*));
+
     *columnSizes = (int*) calloc( matrixRowSize, sizeof(int));
     *returnSize  = matrixRowSize;
     
     int row, col;
-
     for( row=0; row<matrixRowSize; row++  ) {
         result[row] = (int*) calloc( matrixColSizes[row], sizeof(int));
-        score[row]  = (int*) calloc( matrixColSizes[row], sizeof(int));
 
         (*columnSizes)[row] = matrixColSizes[row];
         
-        /*
         for( col=0; col<matrixColSizes[row]; col++ ) {
-            //result[row][col] = -1;
-            //score[row][col] = -1;
+            result[row][col] = -1;
         }
-        */
     }
-    
-    cleanScore( matrixRowSize, matrixColSizes[0], result );
 
     //#pragma omp parallel for
     for( row=0; row<matrixRowSize; row++ ) {
         for( col=0; col<matrixColSizes[row]; col++ ) {
             if( 0==matrix[row][col] ) {
-                
+                // 先把所有 0 起點放到 queue 裡面                
                 result[row][col] = 0;
+                queue[qn].index = row*matrixColSizes[row] + col;
+                queue[qn].dist = 0;
+                qn++;
                 
-                cleanScore( matrixRowSize, matrixColSizes[row], score );
-                testScore( row, col, matrixRowSize, matrixColSizes[row], score, result );
             }
         }
     }
-
+    
+    // 從 queue 開始把迴圈跑完，分數就算完了
+    testScore( matrixRowSize, matrixColSizes[0], result );
     return result;   
 }
